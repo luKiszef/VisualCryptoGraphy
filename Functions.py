3b6ad5dfe54674c136bc829a3940a086ace0ca42
Łukasz Lewański <u55_luklew_waw@technischools.com>
2024-05-06 20:00:50 +0200

    added function to encrypt image

--- Functions.py
+++ Functions.py
@@ -1,3 +1,5 @@
+from PIL import Image
+
 def text_to_binary(text):
     binary_string = ""
     for char in text:
@@ -11,3 +13,21 @@ def binary_to_text(binary_string):
         byte = binary_string[i:i+8]
         text += chr(int(byte, 2))
     return text
+
+def encrypt_image(image_path, binary_message):
+    image = Image.open(image_path)
+    width, height = image.size
+    binary_index = 0
+
+    for y in range(height):
+        for x in range(width):
+            pixel = list(image.getpixel((x, y)))
+            for i in range(3):
+                if binary_index < len(binary_message):
+                    pixel[i] = pixel[i] & ~1 | int(binary_message[binary_index])
+                    binary_index += 1
+            image.putpixel((x, y), tuple(pixel))
+
+    encrypted_image_path = "encrypted_image.png"
+    image.save(encrypted_image_path)
+    return encrypted_image_path
